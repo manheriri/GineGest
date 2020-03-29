65c407e24e2ee7301fe85aa4dbc9c04c1ed17095
vagrant <vagrant@homestead>
2020-03-29 18:11:59 +0000

    modificación de welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="El autor de este proyecto es Manuel Herrero Iriso">
+        <meta name="description" content="El autor de este proyecto titulado GineGest es Manuel Herrero Iriso">
         <title>Laravel</title>
 
         <!-- Fonts -->
