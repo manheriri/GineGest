c6a9595c687855692d98b25bf5d4e8f77e440e7e
vagrant <vagrant@homestead>
2020-03-30 09:12:04 +0000

    commit inicial

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,34 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="surname1" class="col-md-4 col-form-label text-md-right">{{ __('First Surname') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname1" type="text" class="form-control @error('surname1') is-invalid @enderror" name="surname1" value="{{ old('surname1') }}" required autocomplete="surname1" autofocus>
+
+                                @error('surname1')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="surname2" class="col-md-4 col-form-label text-md-right">{{ __('Second Surname') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname2" type="text" class="form-control @error('surname2') is-invalid @enderror" name="surname2" value="{{ old('surname2') }}" required autocomplete="surname2" autofocus>
+
+                                @error('surname2')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -33,6 +61,20 @@
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="dni" class="col-md-4 col-form-label text-md-right">{{ __('DNI') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="dni" type="dni" class="form-control @error('dni') is-invalid @enderror" name="dani" value="{{ old('dni') }}" required autocomplete="dni">
+
+                                @error('dni')
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
@@ -60,6 +102,26 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+
+                        <div class="form-group row">
+                            <label for="userType" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de usuario') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="radio" id="personalSanitario" name="userType" value="personalSanitario" class="@error('userType') is-invalid @enderror" name="userType" value="{{ old('userType') }}" required autocomplete="userType" autofocus>
+                                <label for="personalSanitario">Personal Sanitario</label><br>
+                                <input type="radio" id="paciente" name="userType" value="peciente" class="@error('userType') is-invalid @enderror" name="userType" value="{{ old('userType') }}" required autocomplete="userType" autofocus>
+                                <label for="paciente">Paciente</label><br>
+                                <input type="radio" id="donante" name="userType" value="donante" class="@error('userType') is-invalid @enderror" name="userType" value="{{ old('userType') }}" required autocomplete="userType" autofocus>
+                                <label for="donante">Donante</label><br>
+
+                                @error('userType')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
